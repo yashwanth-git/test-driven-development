d0cb7e4ddc54fe8a32e5991b2121c23a6a16aa7a
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-04-16 00:56:37 -0400

    adds buttonClick check for input and select

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,12 +7,14 @@ describe('Application Test', () => {
     const headEl = screen.getByText(/Vending Machine/i);
     expect(headEl).toBeTruthy();
   });
-  test('On buttonClick, check if amount is entered or not', () => {
+  test('On buttonClick, check if amount is entered or not, check if option is selection', () => {
     const { container } = render(<App />);
     const inputEl = container.querySelector('.amout-paid');
+    const selectEl = container.querySelector('.vending-select');
     const buttonEl = container.querySelector('.pay-btn');
 
     fireEvent.click(buttonEl);
+    expect(selectEl).toBe(true);
     expect(inputEl).not.toBe('');
   });
 });
