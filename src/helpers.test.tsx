098714c25868fea4ec073b1a3059cbe8f05dfc56
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-04-28 13:14:00 -0400

    updates test; best practices

--- src/helpers.test.tsx
+++ src/helpers.test.tsx
@@ -2,18 +2,13 @@ import { getChange } from './helpers';
 
 // Denominations available: 1, 5, 10, 25, 50, 100
 describe('Tests for Helpers', () => {
-  test('getChange(1,1) should equal [] - an empty array', () => {
-    let result = getChange(5, 5);
-    expect(result).toEqual([]);
-  });
-  test('getChange(215, 300) should return [50, 25, 10]', () => {
-    let result = getChange(2.15, 3);
-    let expectedResult = [50, 25, 10];
-    expect(result).toEqual(expectedResult);
-  });
-  test('getChange(125, 200) should return [50, 25]', () => {
-    let result = getChange(1.25, 2);
-    let expectedResult = [50, 25];
-    expect(result).toEqual(expectedResult);
+  describe.each([
+    { a: 1, b: 1, expected: [] },
+    { a: 2.15, b: 3, expected: [50, 25, 10] },
+    { a: 1.25, b: 2, expected: [50, 25] },
+  ])('getChange($a, $b)', ({ a, b, expected }) => {
+    test(`returns ${expected}`, () => {
+      expect(getChange(a, b)).toEqual(expected);
+    });
   });
 });
